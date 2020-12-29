9eda9523fa4d5126ee1179b42be6e058e618d24b
tushar2407 <tushar.mohan2001@gmail.com>
2020-12-29 19:15:41 +0530

    updates

--- tw.py
+++ tw.py
@@ -1,10 +1,16 @@
 import tweepy
 import csv
-consumer_key="AFmamfGy4208xqZNLqIDJiq4D"
-consumer_secret="yLsACdN5yfcV6KiNf4CP3STqSsYMonkiw3j8hTzMGyyqFdGxQM"
+from tqdm import tqdm
+import os
+# import  pandas as pd
+from dotenv import load_dotenv
+load_dotenv()
 
-access_token="1278403366201159680-Eh3s9MCHT28bFzOFlXWKZydtwdGl8L"
-access_token_secret="vHeseRGYju6BS9pfCeAYmjNFF7kGQ5MSAuxhx9MH5pLMc"
+consumer_key=os.getenv("CONSUMER_KEY")
+consumer_secret=os.getenv("CONSUMER_SECRET")
+
+access_token=os.getenv("ACCESS_TOKEN")
+access_token_secret=os.getenv("ACCESS_TOKEN_SECRET")
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -16,15 +22,21 @@ print(api.me().name)
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth,wait_on_rate_limit=True)
-csvFile = open('ua.csv', 'a')
+csvFile = open('ua.csv', 'w')
 csvWriter = csv.writer(csvFile)
-
-for tweet in tweepy.Cursor(api.search,q="#medicaid",count=1000,
+# data = []
+for tweet in tqdm(tweepy.Cursor(api.search,q="#MasterTrailer",count=10000,
                            lang="en",
-                           since="2020-12-03").items():
+                           since="2016-01-01").items()):
     # print (tweet.created_at, tweet.text)
-    print(type(tweet))
-    file = open("tweet.txt", "w+")
-    file.write(str(tweet))
-    break
-    csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
+    # print(type(tweet))
+    # file = open("tweet.txt", "w+")
+    # file.write(str(tweet))
+    # break
+    csvWriter.writerow([
+        tweet.created_at, tweet.text.encode('utf-8'), tweet.user.id, tweet.user.name
+    ])
+#     data.append([
+#         tweet.created_at, tweet.text.encode('utf-8'), tweet.user.id, tweet.user.name
+#     ])
+# df = pd.DataFrame(data, columns = ['CreatedAt', 'tweets', 'user_id', 'user_name'])
