aa5391885642c631daa10df79f463f40a7c339a2
Tushar Mohan <tushar.mohan2001@gmail.com>
2021-05-13 02:01:33 +0530

    Summer21/hw on the way

---
+++ Summer'21/homework1/subreddits.py
@@ -0,0 +1,34 @@
+import requests
+from dotenv import load_dotenv
+load_dotenv()
+import os
+# note that CLIENT_ID refers to 'personal use script' and SECRET_TOKEN to 'token'
+auth = requests.auth.HTTPBasicAuth(os.getenv('REDDIT_CLIENT'), os.getenv('REDDIT_SECRET'))
+
+# here we pass our login method (password), username, and password
+data = {'grant_type': 'password',
+        'username': os.getenv('REDDIT_USERNAME'),
+        'password': os.getenv('REDDIT_PASSWORD')}
+# print(data)
+
+# setup our header info, which gives reddit a brief description of our app
+headers = {'User-Agent': 'MyBot/0.0.1'}
+
+# send our request for an OAuth token
+res = requests.post('https://www.reddit.com/api/v1/access_token',
+                    auth=auth, data=data, headers=headers)
+print(res.json())
+# convert response to JSON and pull access_token value
+TOKEN = res.json()['access_token']
+
+# add authorization to our headers dictionary
+headers = {**headers, **{'Authorization': f"bearer {TOKEN}"}}
+
+# while the token is valid (~2 hours) we just add headers=headers to our requests
+print(requests.get('https://oauth.reddit.com/api/v1/me', headers=headers))
+
+
+res = requests.get("https://oauth.reddit.com/r/python/hot",
+                   headers=headers)
+
+print(res.json())
---
+++ Summer'21/homework1/tweets.py
@@ -0,0 +1,42 @@
+import tweepy
+import csv
+from tqdm import tqdm
+import os
+# import  pandas as pd
+from dotenv import load_dotenv
+load_dotenv()
+
+consumer_key=os.getenv("CONSUMER_KEY")
+consumer_secret=os.getenv("CONSUMER_SECRET")
+
+access_token=os.getenv("ACCESS_TOKEN")
+access_token_secret=os.getenv("ACCESS_TOKEN_SECRET")
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+print(api.me().name)
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth,wait_on_rate_limit=True)
+csvFile = open('india_PalestineBleeding.csv', 'w')
+csvWriter = csv.writer(csvFile)
+# data = []
+for tweet in tqdm(tweepy.Cursor(api.search,q="#PalestineBleeding",count=10000,
+                           lang="en",
+                           since="2020-05-13").items()):
+    # print (tweet.created_at, tweet.text)
+    # print(type(tweet))
+    # file = open("tweet.txt", "w+")
+    # file.write(str(tweet))
+    # break
+    csvWriter.writerow([
+        tweet.created_at, tweet.text.encode('utf-8'), tweet.user.id, tweet.user.name
+    ])
+#     data.append([
+#         tweet.created_at, tweet.text.encode('utf-8'), tweet.user.id, tweet.user.name
+#     ])
+# df = pd.DataFrame(data, columns = ['CreatedAt', 'tweets', 'user_id', 'user_name'])
